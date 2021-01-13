0b826c3c76be56198c261deb98a0120e69fc11f8
Towerbank <59486210+Towerbank@users.noreply.github.com>
2021-01-13 19:21:07 +0300

    Начало работы

--- index.js
+++ index.js
@@ -4,37 +4,37 @@ const task = {
   id: "1",
   text: "выучить html",
   completed: true
-  };
+}
 
-function createListItem(task) {
+// Прошлый код
+function createListItem() {
 
-  const newItem = document.createElement('li');
+  let newItem = document.createElement('li');
   newItem.classList.add('editing');
-  newItem.setAttribute('id', task.id)
-  
-  const newDiv = document.createElement('div');
 
-  const newInput = document.createElement('input');
-  newInput.classList.add('toggle');
-  newInput.setAttribute('type', 'checkbox');
-  newInput.checked = task.completed;
-  
-  const newLabel = document.createElement('label');
-  newLabel.innerHTML = task.text;
-  
-  const newButton = document.createElement('button');
-  newButton.classList.add('destroy');
-  
-  newDiv.append(newInput);
-  newDiv.append(newLabel);
-  newDiv.append(newButton);
-  newItem.append(newDiv);
-  
-  todoList.append(newItem);
-};
-  
-function renderTask() { 
-  createListItem(task)
+  let newDiv = document.createElement('div');
+
+  let newInput = document.createElement('input');	 
+  newInput.classList.add('toggle');	    
+  newInput.setAttribute('type', 'radio');
+
+  let newLabel = document.createElement('label');
+  newLabel.textContent = 'выучить html';
+
+  let newButton = document.createElement('button'); 
+  newButton.classList.add('todo-count');	
+
+  newDiv.append(newInput);	
+  newDiv.append(newLabel);	
+  newDiv.append(newButton);	
+  newItem.append(newDiv);	
+
+  todoList.append(newItem);	
+
 };
 
-renderTask(task)
+createListItem();	
+
+let renderTask = function () { 	
+
+}	
