3a46ad05da281fb4be4d4b40d5585ce9273f9330
Towerbank <59486210+Towerbank@users.noreply.github.com>
2021-01-14 23:15:38 +0300

    другой код в комментарии

--- index.js
+++ index.js
@@ -46,3 +46,25 @@ function renderTasks() {
 
 renderTasks(tasksList);
 
+
+
+// function createListItem() {
+//     let createListItem = `
+//     <li class='editing' id = '${task.id}'>
+//     <div>
+//     <input class='toggle' type='radio'>
+//     <label>${task.text}</label>
+//     <button class='todo-count'></button>
+//     </div>
+//     </li>
+//     `
+//     todoList.innerHTML = createListItem;
+// };
+
+// function renderTask() { 
+
+//   createListItem();
+  
+// };
+
+// todoList.push(renderTask(task));
